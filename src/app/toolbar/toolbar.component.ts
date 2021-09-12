dc3be1c8e0ecc1985807a2a46a21cbc8c28dc51b
erfan hemati <erfanhematig@gmail.com>
2021-09-12 11:18:44 +0430

    some change on toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
+  email = 'erfan@gmail.com'
   constructor() { }
 
   ngOnInit(): void {
